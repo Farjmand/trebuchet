1b2dd481fb73cbe0ad39158575646f81996d8005
Farjmand <64944758+Farjmand@users.noreply.github.com>
2024-06-16 01:08:19 +0500

    update all the functions for all the steps.
    Completed Avent of Code Day 1 task: Trebuchet

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,42 @@ use std::path::Path;
 fn main() {
     // File hosts.txt must exist in the current path
     if let Ok(lines) = read_lines("./map.txt") {
-
+    let mut numbers = Vec::new();
+    let mut result = 0;
         for line in lines.flatten() {
-            let mut digits = Vec::new();
-            for c in line.chars() {
-                if c.is_digit(10) {
-                    digits.push(c)
-                }
+            if let Some(combined_number) = combine_digits_from_each_line(&line) {
+                numbers.push(combined_number)
+            } else {
+                println!("No digits found in line: {}", line);
             }
-            println!("Digits:{:?}", digits)
         }
+        let total_sum = sum_combined_numbers(&numbers);
+        println!("Added all numbers: {:?}", total_sum);
     }
 }
 
-// The output is wrapped in a Result to allow matching on errors.
-// Returns an Iterator to the Reader of the lines of the file.
+fn sum_combined_numbers(numbers: &[i32]) -> i32 {
+    numbers.iter().sum()
+}
+fn combine_digits_from_each_line(line: &str)-> Option<i32> {
+
+    let digits: Vec<_> = line.chars()
+        .filter_map(|c| c.to_digit(10))
+        .collect();
+
+    if digits.len() >= 2 {
+        let first = digits[0] as i32;
+        let last = digits[digits.len() - 1] as i32;
+        Some(first * 10 + last)
+    } else if digits.len() == 1 {
+
+        let first = digits[0] as i32;
+        Some(first * 10 + first)
+    } else {
+        None
+    }
+}
+
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     where P: AsRef<Path>, {
     let file = File::open(filename)?;
